55a3cbdaaf8b1eacea2d2b029a419a184b7b1eac
frank <frank@imafidonfrank2015@gmail.com>
2025-07-06 08:09:51 -0700

    first commit

---
+++ backend_app/models.py
@@ -0,0 +1,53 @@
+from django.db import models
+
+class RegisterUser(models.Model):
+    username = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    accesslevel = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.username
+
+class Item(models.Model):
+    itemname = models.CharField(max_length=255)
+    description = models.TextField(default="")
+
+    def __str__(self):
+        return self.itemname
+
+class Suppliers(models.Model):
+    suppliername = models.CharField(max_length=255)
+    address = models.TextField(default="")
+
+    def __str__(self):
+        return self.suppliername
+
+class Inventory(models.Model):
+    item = models.CharField(max_length=255)
+    supplier = models.TextField()
+    qty = models.IntegerField()
+    cost = models.FloatField()
+    date = models.CharField(max_length=255)
+    staff = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.item
+
+class Sales(models.Model):
+    item = models.CharField(max_length=255)
+    qty = models.FloatField()
+    price = models.FloatField()
+    total = models.FloatField()
+    date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.item
+
+class Expense(models.Model):
+    expensename = models.CharField(max_length=255)
+    expenseamount = models.FloatField()
+    expensestaff = models.CharField(max_length=255)
+    date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.expensename
