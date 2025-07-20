65143ec98b24bbc1b051ca57ec6fa3e8b406fb4a
frank <frank@imafidonfrank2015@gmail.com>
2025-07-20 13:37:10 -0700

    first commit

--- backend_app/admin.py
+++ backend_app/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
-from .models import RegisterUser
+from .models import RegisterUser, Price, Item, Inventory, Suppliers, Sales, Expense
 
 admin.site.register({RegisterUser})
